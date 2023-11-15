51a3b45050346c8d143d86ff2729d9abba7106f9
anoooj0 <130328624+anoooj0@users.noreply.github.com>
2023-11-15 23:23:29 +0000

    more dom manipulation

--- profile.js
+++ profile.js
@@ -8,9 +8,15 @@ button.addEventListener("click", function() {
 
     // 1. Updated the innerHTML of profileName to your name or a made up one.
 
+  profileName.innerHTML = "The Batman"
+
     // 2. Updated the src of profileImage a picture of you or one that makes sense.
 
+  profileImage.src = "https://www.indiewire.com/wp-content/uploads/2021/08/Robert-Pattinson-Batman.jpeg"
+
     // 3. Updated the src of main to a photo that's be your first post.
+
+  main.src = "https://www.watchmojo.com/uploads/thumbs720/WM-CB-Batman-Vs-Joker-Complete-Story-Explained_H2Y0C2-ALT-1.jpg"
 
 
 });
